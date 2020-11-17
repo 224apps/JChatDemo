07b6cba231d79735442ca00698b8099625d23f23
224Apps <224apps@gmail.com>
2020-11-17 14:21:44 -0500

    Sign Up Logic

--- JChatDemo/SignupViewController.swift
+++ JChatDemo/SignupViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import Firebase
 
+
 class SignupViewController: UIViewController {
     
     @IBOutlet weak var titleTextLabel: UILabel!
@@ -48,13 +49,26 @@ class SignupViewController: UIViewController {
     }
 
     @IBAction func signUpButtonDidTapped(_ sender: Any) {
-        Auth.auth().createUser(withEmail: "test@gmail.com", password: "test123") { (authDataResult, error) in
+        Auth.auth().createUser(withEmail: "test3@gmail.com", password: "test123") { (authDataResult, error) in
             if error != nil {
                 print(error!.localizedDescription)
                 return
             }
             if let authData = authDataResult {
                 print(authData.user.email!)
+                let dict: [String: Any] = [
+                    "user" : authData.user.uid,
+                    "email" : authData.user.email!,
+                    "profileImageUrl" : "",
+                    "status" : "Welcome to JChat"
+                ]
+                
+                Database.database().reference().child("users").child(authData.user.uid).updateChildValues(dict) { (err, ref) in
+                    if err == nil {
+                        print("Done")
+                    }
+                    
+                }
             }
         }
     }
