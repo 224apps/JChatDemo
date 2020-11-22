8333a29a30e248621b155eb42cc4b69739cbd5d1
224Apps <224apps@gmail.com>
2020-11-22 01:43:13 -0500

    removed boiler place code and implemented a searchResultControler

--- JChatDemo/Users/PeopleTableViewController.swift
+++ JChatDemo/Users/PeopleTableViewController.swift
@@ -7,14 +7,31 @@
 
 import UIKit
 
-class PeopleTableViewController: UITableViewController {
+class PeopleTableViewController: UITableViewController, UISearchResultsUpdating{
     
     var users: [User] = []
+    var searchController : UISearchController  = UISearchController(searchResultsController: nil)
+    var searchResults: [User] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupSearchBarController()
+        setupNavigationBar()
         ObserveUsers()
+    }
+    
+    func setupSearchBarController(){
+        searchController.searchResultsUpdater  = self
+        searchController.searchBar.placeholder = "Search users"
+        searchController.searchBar.barTintColor =  UIColor.white
+        navigationItem.hidesSearchBarWhenScrolling = false
+        navigationItem.searchController = searchController
+    }
+    
+    func setupNavigationBar(){
+        navigationItem.title = "People"
+        navigationController?.navigationBar.prefersLargeTitles = true
+
     }
     
     func  ObserveUsers(){
@@ -23,65 +40,33 @@ class PeopleTableViewController: UITableViewController {
             self.tableView.reloadData()
         }
     }
-
+    func updateSearchResults(for searchController: UISearchController) {
+        if searchController.searchBar.text == nil || searchController.searchBar.text!.isEmpty {
+            view.endEditing(true)
+        }else{
+            let textLowercased = searchController.searchBar.text!.lowercased()
+            filterContent(for: textLowercased)
+        }
+    }
+    
+    func filterContent(for searchText: String){
+        searchResults = self.users.filter {
+            return $0.username.lowercased().range(of: searchText) != nil
+        }
+    }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return self.users.count
+        return searchController.isActive ? searchResults.count : self.users.count
     }
 
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: IDENTIFIER_CELL_PEOPLE, for: indexPath) as! UserTableViewCell
-        cell.loadData(self.users[indexPath.row])
+        let user = searchController.isActive ? searchResults[indexPath.row] : self.users[indexPath.row]
+        cell.loadData(user)
         return cell
     }
+    
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 94
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
